c6eee17afda7d7a69853a7dba20deaf6b5821a86
NoobMaester <joshstilez@gmail.com>
2022-03-20 23:36:29 +0200

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from '../assets/eth-logo-1.png'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className='logo'>
+            <img src={logo} alt="logo" />
+            <h4>ETH Black 1.0</h4>
+        </div>
+
+        <div className='nav'>
+            <a href="/#">Home</a>
+            <a href="/#">About</a>
+            <a href="/#">Service</a>
+            <a href="/#">Token</a>
+            <a href="/#">Dashboard</a>
+            <a href="/#">Contact</a>
+            <a href="/#" className='btn'>Login</a>
+            <a href="/#" className='btn'>Signup</a>
+            
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Information.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import info from '../assets/info-1.png';
+
+const Information = () => {
+  return (
+    <div className='info'>
+        <div className='content'>
+            <h3>
+                "Investing has the best results when you learn from nature"
+            </h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias dolores magni consectetur dignissimos perferendis quo impedit? Repudiandae quasi ex minima suscipit ut.</p>
+
+            <div className='buttons'>
+                <button className='btn-prime'>Learn more</button>
+                <button className='btn-sec'>Watch Demo</button>
+            </div>
+        </div>
+        
+        <img src={info} alt="info" />
+    </div>
+  )
+}
+
+export default Information;
